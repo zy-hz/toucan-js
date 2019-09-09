50882cc1f005d4abce4e968bdaa996a8d50983cd
zy <yzhang@tcomchina.com>
2019-09-09 23:16:19 +0800

    c

--- libs/toucan-page-spider/index.js
+++ libs/toucan-page-spider/index.js
@@ -1,22 +1,52 @@
-/* eslint-disable no-unused-vars */
 const ToucanPageSpider = require('./_base-page-spider');
+const _ = require('lodash');
 
 // 大嘴鸟的蜘蛛工厂
 class ToucanSpiderFactory {
 
     // 创造蜘蛛
-    createSpider({
-        // 触发获取任务的回掉
-        onGetTask,
-        // 蜘蛛的名称         
-        spiderName,
-        // 空闲的时候，暂停的时间
-        idleSleep
-    } = {}) {
+    createSpider(
+        // 任务选项
+        {
+            // 第一优先：蜘蛛的类型，http - http协议蜘蛛, browser - 浏览器蜘蛛
+            spiderType = '',
+            // 第二优先：任务目标，例如：ali,ali-1688,jd
+            taskTarget = '',
+            // 第三优先：任务的链接
+            taskUrl = '',
 
-        const opt = Object.assign(arguments[0], {});
-        return new ToucanPageSpider(opt);
+        },
+        // 蜘蛛的参数
+        spiderOption = {}
+    ) {
+
+        let spiderClass = {};
+
+        if (!_.isEmpty(spiderType)) spiderClass = createSpiderBySpiderType(spiderType)
+
+        if (_.isEmpty(spiderClass) && !_.isEmpty(taskTarget)) spiderClass = createSpiderByTarget(taskTarget);
+
+        if (_.isEmpty(spiderClass) && !_.isEmpty(taskUrl)) spiderClass = createSpiderByUrl(taskUrl);
+
+        if(_.isEmpty(spiderClass)) spiderClass = ToucanPageSpider;
+        
+        const opt = Object.assign(spiderOption, { spiderType });
+        return new spiderClass(opt);
     }
+}
+
+// 根据蜘蛛的类型创建蜘蛛
+function createSpiderBySpiderType(spiderType) {
+    return require('./_base-page-spider');
+}
+
+// 根据目标创建蜘蛛
+function createSpiderByTarget(targetName) {
+    return require('./_base-page-spider');
+}
+
+function createSpiderByUrl(url) {
+    return require('./_base-page-spider');
 }
 
 module.exports = new ToucanSpiderFactory();
--- test/toucan-page-spider/test-spider.js
+++ test/toucan-page-spider/test-spider.js
@@ -5,10 +5,10 @@ const sipderFactory = require('../../libs/toucan-page-spider');
 describe('page spider 基础功能测试 temp', () => {
 
     it('spider factory create 测试', () => {
-        const opt = {spiderName : '我是测试蜘蛛',idleSleep :231};
-        const spider = sipderFactory.createSpider(opt);
+        const spiderOption = { spiderName: '我是测试蜘蛛', idleSleep: 231 };
+        const spider = sipderFactory.createSpider({}, spiderOption);
 
-        expect(spider.spiderName).to.be.eq(opt.spiderName);
-        expect(spider.idleSleep).to.be.eq(opt.idleSleep);
+        expect(spider.spiderName).to.be.eq(spiderOption.spiderName);
+        expect(spider.idleSleep).to.be.eq(spiderOption.idleSleep);
     });
 });
