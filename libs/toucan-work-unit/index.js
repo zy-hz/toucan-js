c0745ea39c53585d77d62580d0a979d63275bd0d
zy <yzhang@tcomchina.com>
2019-09-12 14:28:43 +0800

    c

--- libs/toucan-work-unit/index.js
+++ libs/toucan-work-unit/index.js
@@ -3,32 +3,17 @@
 //
 // 功能说明：
 // 1. 以任务为工作内容
-// 2. 设置工作单元资料
-//   - 单元名称
-//   - 单元别名
-//   - 单元描述
-//   - 单元标识
-//   - 单元编号
-//   - 单元地址
-// 3. 纪录自身的运行状态
-//   - 开始工作时间
-//   - 当前工作状态，激活中，空闲中，错误中
-//   - 当前运行任务的开始时间
-//   - 当前运行任务的描述
-//   - 当前运行任务的持续时间
-//   - 上个运行任务的开始时间
-//   - 上个运行任务的描述
-//   - 上个运行任务的持续时间
-//   - 上个运行任务的是否成功
-//   - 累计工作时间
-//   - 累计运行任务的时间
-//   - 累计空闲时间
-//   - 累计任务总数，成功数量，失败数量
+// 2. 设置工作单元资料 unitInfo
+// 3. 纪录自身的运行状态 workInfo
 
 const uuid = require('uuid/v1');
+const _ = require('lodash');
+
 class ToucanWorkUnit {
 
     constructor(unitInfo = {}) {
+
+        // 单元资料，构造时确定，以后不会发生变化
         this.unitInfo = Object.assign({
             // 单元名称
             unitName: '',
@@ -43,7 +28,48 @@ class ToucanWorkUnit {
             // 单元地址
             unitAddress: ''
         }, unitInfo);
+
+        // 工作信息
+        this.__work__ = {
+            //   - 开始工作时间
+            unitStartTime: _.now(),
+            //   - 当前工作状态，激活中，空闲中，错误中
+            unitStatus: 'idle',
+            //   - 上个运行任务的开始时间
+            lastTaskBeginTime: 0,
+            //   - 上个运行任务的结束时间
+            lastTaskEndTime: 0,
+            //   - 上个运行任务的持续时间
+            lastTaskSpendTime: 0,
+            //   - 上个运行任务的是否成功
+            lastTaskSuccessful: false,
+            //   - 上个运行任务的唯一标识
+            lastTaskId: '',
+            //   - 上个运行任务的描述
+            lastTaskDescription: '',
+            //   - 累计工作时间
+            unitDuratioinTime: 0,
+            //   - 累计运行任务的时间
+            totalUnitActiveTime: 0,
+            //   - 累计空闲时间
+            totalUnitIdleTime: 0,
+            //   - 累计错误时间
+            totalUnitErrorTime: 0,
+            //   - 累计任务总数            
+            totalExecuteTaskCount: 0,
+            //   - 累计成功任务数量
+            totalDoneTaskCount: 0,
+            //   - 累计失败任务数量
+            totalErrorTaskCount: 0,
+        };
+
     }
+
+    get workInfo() {
+        const unitDuratioinTime = _.now() - this.__work__.unitStartTime;
+        return Object.assign(this.__work__, { unitDuratioinTime });
+    }
+
 }
 
 module.exports = ToucanWorkUnit;
--- test/toucan-gather-cell/test-gather-cell.js
+++ test/toucan-gather-cell/test-gather-cell.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-undef */
 const expect = require('chai').expect;
 const _ = require('lodash');
+const { sleep } = require('../../libs/toucan-utility');
 const ToucanGatherCell = require('../../libs/toucan-gather-cell');
 
 describe('ToucanGatherCell 构造测试 temp', () => {
 
-    it('', () => {
+    it('', async () => {
+        // 固定时间
+        const theTime = _.now()
         const gatherCell = new ToucanGatherCell({ unitInfo: { unitNo: 'N1988' } });
-        const { unitInfo } = gatherCell;
+        const { unitInfo, workInfo } = gatherCell;
 
         expect(_.isNil(gatherCell), '对象不能为空').to.be.false;
         expect(_.isNil(unitInfo), '单元资料对象不能为空').to.be.false;
@@ -15,5 +18,17 @@ describe('ToucanGatherCell 构造测试 temp', () => {
         expect(unitInfo.unitNo, '单元编号为 N1988').to.be.eq('N1988');
         expect(_.isEmpty(unitInfo.unitId), '单元标识为 自动uuid').to.be.false;
 
+        expect(_.isNil(workInfo), '工作信息不能为空').to.be.false;
+        expect(workInfo.unitStartTime, '单元启动时间 小于').to.be.greaterThan(theTime);
+        expect(workInfo.unitStartTime, '单元启动时间 大于0').to.be.lessThan(_.now());
+
+        // 测试启动时间
+        await sleep(100);
+        let d1 = gatherCell.workInfo.unitDuratioinTime
+        expect(d1, '经历的时间 大于 0').to.be.greaterThan(0);
+
+        await sleep(100);
+        let d2 = gatherCell.workInfo.unitDuratioinTime
+        expect(d2, 'd2 经历的时间 大于 d1').to.be.greaterThan(d1);
     });
 })
