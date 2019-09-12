6aee115a0884317c5ba79b407bb907c04f29e820
zy <yzhang@tcomchina.com>
2019-09-12 23:40:19 +0800

    c

--- test/toucan-utility/test-status.js
+++ test/toucan-utility/test-status.js
@@ -16,7 +16,7 @@ describe('StatusGroup 测试 temp', () => {
         expect(statusGroup.isSuspend, 'isSuspend 为false').to.be.false;
 
         await sleep(100);
-        const secondTime = _.now()
+        const secondTime = _.now();
         statusGroup.updateStatus(StatusCode.actived, secondTime);
 
         expect(statusGroup.status, '默认为 actived').to.be.eq(StatusCode.actived);
@@ -27,5 +27,13 @@ describe('StatusGroup 测试 temp', () => {
         expect(statusGroup.lastIdleChangeTime, '上次空闲变更时间为 theTime').to.be.eq(theTime);
         expect(statusGroup.lastActivedChangeTime, '上次激活变更时间为 secondTime').to.be.eq(secondTime);
         expect(statusGroup.idleDurationTime, 'idle 持续时间').to.be.eq(secondTime - theTime);
+
+        await sleep(100);
+        const thirdTime = _.now();
+        statusGroup.updateStatus(StatusCode.idle, thirdTime);
+
+        expect(statusGroup.lastIdleChangeTime, '上次空闲变更时间为 thirdTime').to.be.eq(thirdTime);
+        expect(statusGroup.lastActivedChangeTime, '上次激活变更时间为 secondTime').to.be.eq(secondTime);
+        expect(statusGroup.activedDurationTime, 'idle 持续时间').to.be.eq(thirdTime - secondTime);
     })
 });
