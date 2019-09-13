5e8f33c4c5bbe8d1dccfd028aebd79c261c1b8ea
zy <yzhang@tcomchina.com>
2019-09-13 12:06:58 +0800

    c

--- test/toucan-mq-visitor/test-mq-visitor.js
+++ test/toucan-mq-visitor/test-mq-visitor.js
@@ -4,10 +4,13 @@ const _ = require('lodash');
 
 const mqVisitorFactory = require('../../libs/toucan-mq-visitor');
 
-describe('ToucanMQVisitorFactory 测试 temp',()=>{
+describe('ToucanMQVisitorFactory 测试 temp', () => {
 
-    it('create base visitor',()=>{
-        const baseMQVisitor = mqVisitorFactory.create();
-        expect(_.isNil(baseMQVisitor),'base visitor 构造不能为空').to.be.false;
+    it('create base visitor', () => {
+        const option = { op1: 1, op2: 'abc' };
+        const baseMQVisitor = mqVisitorFactory.create(option);
+
+        expect(_.isNil(baseMQVisitor), 'base visitor 构造不能为空').to.be.false;
+        expect(baseMQVisitor.option, 'base visitor  option比较').to.be.deep.eq(option);
     })
 });
