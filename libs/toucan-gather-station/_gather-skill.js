a34512f7ebbd0d62efcd7cc7625ea5a8fe082b1c
zy <yzhang@tcomchina.com>
2019-09-16 19:00:04 +0800

    c

--- libs/toucan-gather-station/_gather-skill.js
+++ libs/toucan-gather-station/_gather-skill.js
@@ -6,9 +6,16 @@ const _ = require('lodash');
 // 创建能力模板
 function createGatherSkillTemplate(maxCount, gatherSkills) {
 
+    // 根据能力的期望大小排序
+    gatherSkills = _.orderBy(gatherSkills,compareSkillCapability);
+
     return _.map(gatherSkills, (skill) => {
 
     })
+}
+
+function compareSkillCapability(a,b){
+    console.log(a,b)
 }
 
 module.exports = { createGatherSkillTemplate }
