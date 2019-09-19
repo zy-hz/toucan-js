80238d4544f7fe2479f0b6b48ed00ff03a2b0867
zy <yzhang@tcomchina.com>
2019-09-19 13:49:34 +0800

    c

--- test/toucan-gather-station/test-gather-station-v1.js
+++ test/toucan-gather-station/test-gather-station-v1.js
@@ -7,98 +7,102 @@ const lib = require("rewire")('../../libs/toucan-gather-station/_gather-statioin
 const buildGatherCellPool = lib.__get__('buildGatherCellPool');
 const buildGatherCells = lib.__get__('buildGatherCells');
 
-describe('GatherStationV1 测试 ', () => {
-    const cfgFileName = `${__dirname}/gsconfig.json`;
+describe('GatherStationV1 综合测试', () => {
 
-    it('读取配置文件 ', () => {
-        const gs = new ToucanGatherStation(cfgFileName);
-        expect(_.isNil(gs)).to.be.false;
+    describe('GatherStationV1 测试 ', () => {
+        const cfgFileName = `${__dirname}/gsconfig.json`;
 
-        runExpect4GatherSkill(gs.stationConfig.gatherSkill);
+        it('读取配置文件 ', () => {
+            const gs = new ToucanGatherStation(cfgFileName);
+            expect(_.isNil(gs)).to.be.false;
+
+            runExpect4GatherSkill(gs.stationConfig.gatherSkill);
+        });
+
+        it('初始化-不启动', async () => {
+            const gs = new ToucanGatherStation(cfgFileName);
+            gs.stationConfig.autoStart = false;
+            await gs.init();
+
+            expect(gs.workInfo.unitStatus.isIdle).to.be.true;
+            expect(gs.unitInfo.unitAddress).is.not.empty;
+        });
+
+        it('初始化-启动', async () => {
+            const gs = new ToucanGatherStation(cfgFileName);
+            await gs.init();
+
+            expect(gs.workInfo.unitStatus.isActived, '状态应为启动').to.be.true;
+            expect(gs.unitInfo.unitName, '期望是机器名称').to.be.eq('DESKTOP-19SS3KS');
+            expect(gs.unitInfo.unitAddress).is.not.empty;
+
+            await gs.stop();
+        });
     });
 
-    it('初始化-不启动', async () => {
-        const gs = new ToucanGatherStation(cfgFileName);
-        gs.stationConfig.autoStart = false;
-        await gs.init();
+    describe('GatherStationV1 内部方法测试 ', () => {
 
-        expect(gs.workInfo.unitStatus.isIdle).to.be.true;
-        expect(gs.unitInfo.unitAddress).is.not.empty;
-    });
+        it('buildGatherCellPool 参数异常', () => {
+            try {
+                buildGatherCellPool();
+            }
+            catch (e) {
+                expect(e.argName).to.be.eq('maxGatherCellCount');
+            }
 
-    it('初始化-启动', async () => {
-        const gs = new ToucanGatherStation(cfgFileName);
-        await gs.init();
+            try {
+                buildGatherCellPool({ maxGatherCellCount: 5 });
+            }
+            catch (e) {
+                expect(e.argName).to.be.eq('gatherCells');
+            }
+        });
 
-        expect(gs.workInfo.unitStatus.isActived, '状态应为启动').to.be.true;
-        expect(gs.unitInfo.unitName, '期望是机器名称').to.be.eq('DESKTOP-19SS3KS');
-        expect(gs.unitInfo.unitAddress).is.not.empty;
-
-        await gs.stop();
-    });
-});
-
-describe('GatherStationV1 内部方法测试 ', () => {
-
-    it('buildGatherCellPool 参数异常', () => {
-        try {
-            buildGatherCellPool();
-        }
-        catch (e) {
-            expect(e.argName).to.be.eq('maxGatherCellCount');
-        }
-
-        try {
-            buildGatherCellPool({ maxGatherCellCount: 5 });
-        }
-        catch (e) {
-            expect(e.argName).to.be.eq('gatherCells');
-        }
-    });
-
-    it('buildGatherCells 1模板2实例测试', () => {
-        const skill = {
-            skillName: 'http采集',
-            skillKeys: ['cm.http', 'cm.browser'],
-            skillCapability: 2,
-        }
-        const gcs = buildGatherCells(skill, 9, { unitId: 'test',unitAddress:'my.add.111' });
-        expect(gcs).to.have.lengthOf(2);
-        expect(gcs[1].mqVisitor).is.not.null;
-        expect(gcs[1].unitInfo.unitName).to.be.eq(skill.skillName);
-        expect(gcs[1].unitInfo.unitId).to.be.eq('test-10');
-        expect(gcs[1].unitInfo.unitNo).to.be.eq('02');
-        expect(gcs[1].unitInfo.unitAddress).to.be.eq('my.add.111');
-
-    });
-
-    it('buildGatherCellPool 结果测试 ', () => {
-        const skills = {
-            maxGatherCellCount: 5,
-            gatherCells: [{
+        it('buildGatherCells 1模板2实例测试', () => {
+            const skill = {
                 skillName: 'http采集',
-                skillKeys: ['cm.http'],
+                skillKeys: ['cm.http', 'cm.browser'],
                 skillCapability: 2,
-            },
-            {
-                skillName: 'browsers采集',
-                skillKeys: ['cm.browser'],
-                skillCapability: -1,
-            }]
-        }
+            }
+            const gcs = buildGatherCells(skill, 9, { unitId: 'test', unitAddress: 'my.add.111' });
+            expect(gcs).to.have.lengthOf(2);
+            expect(gcs[1].mqVisitor).is.not.null;
+            expect(gcs[1].unitInfo.unitName).to.be.eq(skill.skillName);
+            expect(gcs[1].unitInfo.unitId).to.be.eq('test-10');
+            expect(gcs[1].unitInfo.unitNo).to.be.eq('02');
+            expect(gcs[1].unitInfo.unitAddress).to.be.eq('my.add.111');
 
-        const gcPool = buildGatherCellPool(skills);
-        expect(gcPool, '队列长度异常').to.have.lengthOf(skills.maxGatherCellCount);
+        });
 
-        const gc1 = gcPool.findIndex(0);
-        const gc5 = gcPool.findIndex(4);
+        it('buildGatherCellPool 结果测试 ', () => {
+            const skills = {
+                maxGatherCellCount: 5,
+                gatherCells: [{
+                    skillName: 'http采集',
+                    skillKeys: ['cm.http'],
+                    skillCapability: 2,
+                },
+                {
+                    skillName: 'browsers采集',
+                    skillKeys: ['cm.browser'],
+                    skillCapability: -1,
+                }]
+            }
 
-        expect(gc1.unitInfo.unitName).to.be.eq('http采集');
-        expect(gc5.unitInfo.unitName).to.be.eq('browsers采集');
-        expect(gc1.gatherMQ).to.not.eq(gc5.gatherMQ);
+            const gcPool = buildGatherCellPool(skills);
+            expect(gcPool, '队列长度异常').to.have.lengthOf(skills.maxGatherCellCount);
 
+            const gc1 = gcPool.findIndex(0);
+            const gc5 = gcPool.findIndex(4);
+
+            expect(gc1.unitInfo.unitName).to.be.eq('http采集');
+            expect(gc5.unitInfo.unitName).to.be.eq('browsers采集');
+            expect(gc1.gatherMQ).to.not.eq(gc5.gatherMQ);
+
+        });
     });
-});
+
+})
 
 
 function runExpect4GatherSkill(gatherSkill) {
