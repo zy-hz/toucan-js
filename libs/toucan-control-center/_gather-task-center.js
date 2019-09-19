41f7b5a2a8716d8924dab6382018c60a2d598e83
zy <yzhang@tcomchina.com>
2019-09-19 13:31:48 +0800

    c

--- libs/toucan-control-center/_gather-task-center.js
+++ libs/toucan-control-center/_gather-task-center.js
@@ -1,8 +1,33 @@
+//
+// 采集任务管理中心
+//
 
-class GatherTaskCenter {
+const { ToucanWorkUnit } = require('../toucan-work-unit')
+const { StatusCode } = require('../toucan-utility');
+const mqFactory = require('../toucan-message-queue');
+
+class GatherTaskCenter extends ToucanWorkUnit {
+
+    constructor(cfgFileName = '') {
+        super();
+    }
 
     async start() {
+        await this._init();
+        await this.taskMQ.connect();
 
+        this.workInfo.unitStatus.updateStatus(StatusCode.actived);
+    }
+
+    // 初始化
+    async _init() {
+        // 创建消息队列
+        this.taskMQ = mqFactory.createTaskMQ('rabbit');
+    }
+
+    async stop() {
+        await this.taskMQ.disconnect();
+        this.workInfo.unitStatus.updateStatus(StatusCode.idle);
     }
 }
 
--- test/toucan-control-center/test-gather-task-center.js
+++ test/toucan-control-center/test-gather-task-center.js
@@ -2,15 +2,29 @@
 const expect = require('chai').expect;
 const GatherTaskCenter = require('../../libs/toucan-control-center/_gather-task-center');
 
-describe('GatherTaskCenter 启动测试', () => {
+describe('GatherTaskCenter 综合测试 temp', () => {
 
-    it('构造', () => {
-        const gtc = new GatherTaskCenter();
+    describe('基础', () => {
 
-        expect(gtc).is.not.null;
+        it('构造', () => {
+            const gtc = new GatherTaskCenter();
+
+            expect(gtc).is.not.null;
+        });
+
+        it('启动', async () => {
+            const gtc = new GatherTaskCenter();
+            await gtc.start();
+
+            // 站点处于激活状态
+            expect(gtc.workInfo.unitStatus.isActived).to.be.true;
+
+            await gtc.stop();
+            // 站点处于激活状态
+            expect(gtc.workInfo.unitStatus.isIdle).to.be.true;
+        });
+
     });
 
-    it('启动', async () => {
 
-    });
 });
