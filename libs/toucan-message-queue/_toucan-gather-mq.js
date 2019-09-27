94f9393dd9159c1c72373eebe00727567e58e4c2
zy <yzhang@tcomchina.com>
2019-09-27 10:50:21 +0800

    c

--- libs/toucan-message-queue/_toucan-gather-mq.js
+++ libs/toucan-message-queue/_toucan-gather-mq.js
@@ -3,6 +3,7 @@
 
 const ToucanBaseMQ = require('./_toucan-base-mq');
 const { sleep } = require('../toucan-utility');
+const _ = require('lodash');
 
 class ToucanGatherMQ extends ToucanBaseMQ {
 
@@ -11,14 +12,32 @@ class ToucanGatherMQ extends ToucanBaseMQ {
     }
 
     // 绑定采集任务的队列
-    // fromQueues - 订阅的队列列表，例如：toucan.cm.http,toucan.sp.com.ali
-    bindTaskQueue(fromQueues = []){
+    // fromQueues
+    // 1. 订阅的队列列表，例如：toucan.cm.http,toucan.sp.com.ali
+    // 2. 多个队列的时候，轮流获得队列中的任务
+    bindTaskQueue(fromQueues = []) {
+        this.__taskBindQueue = _.concat([], fromQueues);
+    }
 
+    // 弹出当前的队列，并把他推入队列的末尾
+    popTaskQueue(toEnd = true) {
+        const first = _.pullAt(this.__taskBindQueue, 0);
+
+        if (toEnd) {
+            // 把第一个元素放到最后
+            this.__taskBindQueue = _.concat(this.__taskBindQueue, first);
+        } else {
+            // 把第一个元素放到第一个
+            this.__taskBindQueue = _.concat(first, this.__taskBindQueue);
+        }
+
+        return _.head(first);
     }
 
     // 订阅采集任务
-
     async subscribeTask() {
+        // 获得订阅的队列名称，例如：toucan.cm.http
+        const queue = this.popTaskQueue();
 
         await this.mqVisitor.receive(async (msg) => {
 
@@ -26,9 +45,7 @@ class ToucanGatherMQ extends ToucanBaseMQ {
             await sleep(1000);
             return true;
 
-        }, {
-            queue: 'toucan.cm.http'
-        })
+        }, { queue })
     }
 }
 
--- test/toucan-message-queue/toucan-gather-mq.test.js
+++ test/toucan-message-queue/toucan-gather-mq.test.js
@@ -3,11 +3,26 @@ const expect = require('chai').expect;
 const mqFactory = require('../../libs/toucan-message-queue');
 
 
-describe('ToucanGatherMQ 测试', () => {
-
-    it('bindTaskQueue temp', () => {
+describe('ToucanGatherMQ 测试 temp', () => {
+    const fromQueues = ['toucan.cm.http', 'toucan.cm.browse', 'toucan.sp.com.sohu.news', 'toucan.sp.com.sohu'];
+    it('bindTaskQueue ', () => {
         const gatherMQ = mqFactory.createGatherMQ('rabbit');
-        gatherMQ.bindTaskQueue()
+        gatherMQ.bindTaskQueue(fromQueues);
+
+        let queue = gatherMQ.popTaskQueue();
+        expect(queue).to.be.eq('toucan.cm.http');
+
+        queue = gatherMQ.popTaskQueue();
+        expect(queue).to.be.eq('toucan.cm.browse');
+
+        queue = gatherMQ.popTaskQueue();
+        expect(queue).to.be.eq('toucan.sp.com.sohu.news');
+
+        queue = gatherMQ.popTaskQueue();
+        expect(queue).to.be.eq('toucan.sp.com.sohu');
+
+        queue = gatherMQ.popTaskQueue();
+        expect(queue).to.be.eq('toucan.cm.http');
     });
 
 
