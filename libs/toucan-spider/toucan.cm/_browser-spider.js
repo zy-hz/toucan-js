b81711535d7476ce6675a578f802c2ff7a952eba
zy <yzhang@tcomchina.com>
2019-10-08 14:54:11 +0800

    c

--- libs/toucan-spider/toucan.cm/_browser-spider.js
+++ libs/toucan-spider/toucan.cm/_browser-spider.js
@@ -3,9 +3,16 @@
 // 
 
 const { ToucanBaseSpider } = require('../_base-spider');
+const pageFetchFactory = require('../../toucan-page-fetch');
 
 class ToucanBrowserSpider extends ToucanBaseSpider {
+    constructor(option = {}) {
+        // 继承基类的创建参数
+        super(option);
 
+        // 创建request类型的页面抓手
+        this.pageFetch = pageFetchFactory.createFetch({ fetchType: 'webpage' });
+    }
 }
 
 module.exports = ToucanBrowserSpider;
