128c80a4632313e40c6ab4d304f4baa4f2cd31ab
zy <yzhang@tcomchina.com>
2019-10-08 10:44:05 +0800

    c

--- libs/toucan-job/_base-task-job.js
+++ libs/toucan-job/_base-task-job.js
@@ -5,8 +5,29 @@ class TaskJob extends ToucanJob {
 
     // 记录爬行任务完成一个页面的日志
     logTaskPageDone(task, page) {
-        this.log(task)
+        if (page.hasException) {
+            const msg = buildTaskPageDoneError(task, page);
+            this.warn(msg);
+        }
+        else {
+            const msg = buildTaskPageDoneSuccess(task, page);
+            this.log(msg);
+        }
     }
+}
+
+function buildTaskPageDoneError(task, page) {
+    const taskInfo = buildTaskPageProcess(task, '采集失败', page);
+    return taskInfo;
+}
+
+function buildTaskPageDoneSuccess(task, page) {
+    const taskInfo = buildTaskPageProcess(task, '采集成功', page);
+    return taskInfo;
+}
+
+function buildTaskPageProcess(task, msg, { pageLayerIndex, pageSpendTime } = {}) {
+    return `任务${task.targetUrl}第${task.taskDonePageCount + task.taskErrorPageCount}页/第${pageLayerIndex + 1}层${msg}，用时${pageSpendTime}毫秒`;
 }
 
 module.exports = { TaskJob }
