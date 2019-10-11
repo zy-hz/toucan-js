adeabe39ed470981e951d4db65f0658d2a406c37
zy <yzhang@tcomchina.com>
2019-10-11 11:47:40 +0800

    c

--- libs/toucan-mq-visitor/_file-mq-visitor.js
+++ libs/toucan-mq-visitor/_file-mq-visitor.js
@@ -8,6 +8,13 @@ const _ = require('lodash');
 
 class FileMQVisitor extends ToucanMQVisitor {
 
+    constructor(optioins = {}) {
+        super(optioins);
+
+        // 创建数据存储
+        this.__dataStorage__ = {};
+    }
+
     // 连接到消息服务器
     async connect() {
     }
@@ -22,13 +29,27 @@ class FileMQVisitor extends ToucanMQVisitor {
     }
 
     // 发送消息
-    async send(content, { exchange, routeKey, queue, options = {} }) {
-        let ok = true;
-        return ok;
+    async send(content, { exchange, routeKey,
+        // queue = 关联指定的文件
+        queue, options = {}
+    }) {
+
+        // 初始化数组
+        const queueName = queue || '_toucan_default_queue';
+        if (_.isNil(this.__dataStorage__[queueName])) this.__dataStorage__[queueName] = []
+
+        // 推入队列
+        this.__dataStorage__[queueName].push({ content, isRead: false });
+        return true;
     }
 
-    async read({ queue, queueOptions, consumeOptions }) {
-        let msg = { content: 'ok' };
+    async read({ queue }) {
+        const q = _.find(this.__dataStorage__, { 'queueName': queue });
+        if (_.isNil(q) || _.isEmpty(q.queueDs)) return false;
+
+        const msg = _.find(q.queueDs, { 'isRead': false });
+        if (_.isNil(msg)) return false;
+
         return msg;
     }
 }
