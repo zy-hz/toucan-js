b07c0e3a9ba739fe6060ad749e72cc45ace35930
zy <yzhang@tcomchina.com>
2019-11-11 09:11:00 +0800

    cache

--- libs/toucan-app/cache.js
+++ libs/toucan-app/cache.js
@@ -15,10 +15,32 @@ function readCacheFromFile(f) {
 
 }
 
+// 缓存写入文件
+function writeCacheToFile(c, f) {
+    // 删除不需要的属性
+    delete c.cacheFileName;
+
+    // 转为json存储
+    const cnt = JSON.stringify(c);
+    fs.writeFileSync(f, cnt);
+}
+
 module.exports = class {
 
     init(f) {
         this.cacheFileName = f;
         _.assign(this, readCacheFromFile(f));
+    }
+
+    set(obj, val) {
+        if (_.isString(obj)) {
+            const newObj = {};
+            newObj[`${obj}`] = val;
+            obj = newObj;
+        }
+
+        _.assign(this, obj);
+
+        writeCacheToFile(_.cloneDeep(this), this.cacheFileName);
     }
 }
--- test/toucan-gather-station/service/cache.test.js
+++ test/toucan-gather-station/service/cache.test.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-undef */
 const cache = require('../../../libs/toucan-gather-station/service/cache');
 const path = require('path');
+const fs = require('fs');
 const expect = require('chai').expect;
 
-describe('temp [测试入口] - cache', () => {
+describe('[测试入口] - cache', () => {
     const cacheFile = path.resolve(`${process.cwd()}`, '.cache', 'test_gs_cache.json');
+    const obj = { testA: 'a', testB: { c1: 1, c2: '我是' } };
+
+    before('', () => {
+        if (fs.existsSync(cacheFile)) fs.unlinkSync(cacheFile);
+    })
 
     it('init', () => {
         cache.init(cacheFile);
@@ -12,8 +18,17 @@ describe('temp [测试入口] - cache', () => {
 
     })
 
-    it('set',()=>{
-        
+    it('set', () => {
+        cache.set({ testA: 'a' });
+        expect(cache.testA).to.be.eq(obj.testA);
+
+        cache.set('testB', obj.testB);
+        expect(cache.testB).to.be.eql(obj.testB);
+
+        // 读取文件
+        const content = fs.readFileSync(cacheFile);
+        const jsonObj = JSON.parse(content);
+        expect(jsonObj, 'json文件和obj一致').to.be.eql(obj);
     })
 
 })
